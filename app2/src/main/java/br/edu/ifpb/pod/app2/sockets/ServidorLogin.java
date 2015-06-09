bb824668ca214d2dd610cccc5e44903efd4e4434
Douglas Gabriel <douglasxgabriel@gmail.com>
2015-06-08 22:29:29 -0300

    finalizando login

--- app2/src/main/java/br/edu/ifpb/pod/app2/sockets/ServidorLogin.java
+++ app2/src/main/java/br/edu/ifpb/pod/app2/sockets/ServidorLogin.java
@@ -1,12 +1,16 @@
 package br.edu.ifpb.pod.app2.sockets;
 
+import br.edu.ifpb.pod.app2.dao.UsuarioPersistivelDAO;
+import br.edu.ifpb.pod.app2.dao.interfaces.DAO;
 import br.edu.ifpb.pod.app2.entidades.UsuarioPersistivel;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintWriter;
+import java.net.MalformedURLException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.URL;
 import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -18,45 +22,55 @@ import java.util.logging.Logger;
 public class ServidorLogin {
 
     private static final int PORT = 12345;
-    private Map<String,UsuarioPersistivel> usuarios;
+    /*TODO pensar numa estratégia para que este objeto fique disponível para 
+    todos os que possivelmente irão manipulá-lo*/
+    private Map<String, UsuarioPersistivel> usuarios;
+    private DAO<UsuarioPersistivel> daoUsuario = new UsuarioPersistivelDAO();
 
     public ServidorLogin(Map<String, UsuarioPersistivel> usuarios) {
         this.usuarios = usuarios;
     }
-    
+
     public void inicialize() throws Exception {
         ServerSocket listener = new ServerSocket(PORT);
         try {
             while (true) {
-               new Comunicador(listener.accept()).start();
+                new Comunicador(listener.accept()).start();
             }
         } finally {
             listener.close();
         }
     }
-    
-    private class Comunicador extends Thread{
+
+    private class Comunicador extends Thread {
 
         private Socket socket;
         private PrintWriter out;
         private InputStream in;
         private String mensagem;
-        
-        public Comunicador(Socket socket){
-            this.socket=socket;
+
+        public Comunicador(Socket socket) {
+            this.socket = socket;
         }
-        
+
         @Override
         public void run() {
             try {
-                out=new PrintWriter(socket.getOutputStream(), true);
+                out = new PrintWriter(socket.getOutputStream(), true);
                 out.println("ACCEPTED");
-                in=socket.getInputStream();
-                mensagem=gerarString();
-                //Logica do token
-                //Retorno o ID
-                
-                
+                in = socket.getInputStream();
+                mensagem = respostaCliente();
+                if (mensagem.startsWith("TOKEN:")) {
+                    String userEmail = recuperarEmail(mensagem.substring(6));
+                    UsuarioPersistivel user
+                            = daoUsuario.buscar(
+                                    userEmail, UsuarioPersistivel.class
+                            );
+                    if (user != null) {
+                        usuarios.put(userEmail, user);
+                        out.println(System.currentTimeMillis()+userEmail);
+                    }
+                }
             } catch (IOException ex) {
                 Logger.getLogger(ServidorLogin.class.getName()).log(Level.SEVERE, null, ex);
             } finally {
@@ -67,14 +81,35 @@ public class ServidorLogin {
             }
         }
 
-        private String gerarString() throws IOException {
-            ByteArrayOutputStream outputStream=new ByteArrayOutputStream();
-            byte[] b=new byte[1];
-            while(in.read(b)!=-1){
+        private String respostaCliente() throws IOException {
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+            byte[] b = new byte[1];
+            while (in.read(b) != -1) {
                 outputStream.write(b);
             }
             return new String(outputStream.toByteArray());
         }
-        
+
+        private String recuperarEmail(String token) throws MalformedURLException, IOException {
+            URL url = new URL(
+                    "https://graph.facebook.com/me?access_token=" + token
+            );
+            ByteArrayOutputStream output = new ByteArrayOutputStream();
+            InputStream input = url.openStream();
+            byte[] b = new byte[1];
+            while ((input.read(b)) != 1) {
+                output.write(b);
+            }
+            //TODO verificar encoding para não quebrar e-mail
+            String[] retorno = new String(output.toByteArray()).split(",");            
+            for (String prop: retorno){
+                if (prop.startsWith("\"email\":")){
+                    prop.replace("\"", "");
+                    return prop.substring(6);
+                }                    
+            }
+            return null;
+        }
+
     }
 }
--- app2/src/main/java/br/edu/ifpb/pod/app2/teste/TesteAcessoFacebook.java
+++ app2/src/main/java/br/edu/ifpb/pod/app2/teste/TesteAcessoFacebook.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.nio.charset.Charset;
 
 /**
  *
@@ -13,17 +14,17 @@ import java.net.URL;
 public class TesteAcessoFacebook {
 
     public static void main(String[] args) throws MalformedURLException, IOException {
-        URL url = new URL("https://graph.facebook.com/me?access_token=CAAK7CyXZCAhgBAJhoiHKdoqTNGULH4mZAhefAgzxswkjPvWTMXZCnvHkzW0OZAbsvZAW7xHmbhbguKjeaqpZAzZB5In0UkDw1tmP720lAFk81tgng0ygZAfcDZBFxkEjuxl1OwlHs9b2tcqCnjZBLTmDK6ZAgZCZCI0oECyqSvpmaIyEB2ae6NAlJlSH0KVIRwYkbvzf6laDw5XlTS8gPtNePHQXT");
+        URL url = new URL("https://graph.facebook.com/me?access_token=CAAK7CyXZCAhgBAOje85g5m5yefhlAfss00ZBQUPAWarYZA6kAvenyEZCFnQZAWSY6NgE2xdOtkHYVAum8ZCnH9vQ1UbHqG3Mc1aXlDlUwguwsJ49EvJLfWIXVl3FuV07baWZAsjqMDQltVXbh00dAUvt2jAd96OBtGSs5tyegqNik10LGZBYFZBbo0ZCvurP7shsgGungbRayPPSDZA5uDAqLDW");
         System.out.println(readUrl(url));
     }
     
     private static String readUrl (URL url) throws IOException{
-        ByteArrayOutputStream output = new ByteArrayOutputStream();
-        InputStream input = url.openStream();
+        ByteArrayOutputStream output = new ByteArrayOutputStream();        
+        InputStream input = url.openStream();        
         byte[] b = new byte[1];
         while ((input.read(b)) != -1){
             output.write(b);
         }
-        return new String (output.toByteArray());
+        return new String(output.toByteArray(), Charset.forName("utf-8"));
     }
 }
